9d148cdb2ab035a936b354817693a02d3b3418f6
giuviglia94 <vigliaturo94@gmail.com>
2019-11-10 22:51:56 +0100

    cambio icona hamburger sul click di apertura e chiusura

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,7 @@
 //  });
 
 //versione alternativa
+$(document).ready(function(){
  $(".dropdown").click(function() {
    //mi salvo tutti i .dropdown presenti in pagina
    var drop = $(".dropdown");
@@ -19,15 +20,26 @@
      //se il drop su cui clicco è uguale all'i-esimo drop ciclato allora il valore attuale del contatore i
      //posso usarlo come indice per risalire al dropdowncontent corispondente
      if ($(this).is(drop.eq(i))) {
-       console.log("entro nell'if");
-      $("dropdown.visible").removeClass("visible");
        $(".dropdowncontent").eq(i).toggle();
      }
    }
 
-
  });
 
  $(".menu-toggle").click(function(){
-     $(".navright").toggleClass("active");
+    $(".navright").toggleClass("active");
+    //cambio l'icona a seconda se si debba chiudere o aprire il menù a scomparsa
+   if ($(".croce").hasClass("invisible")) {
+     $(".hamburger").removeClass("visible").addClass("invisible");
+     $(".croce").removeClass("invisible").addClass("visible");
+
+   }else if ($(".hamburger").hasClass("invisible")) {
+     $(".croce").addClass("invisible");
+     $(".hamburger").removeClass("invisible").addClass("visible");
+
+   }
+
+
    });
+
+});
