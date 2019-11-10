16f7711c02be01d36cd292407ce39f30987cb749
giuviglia94 <vigliaturo94@gmail.com>
2019-11-10 18:48:58 +0100

    versione con codice js alternativo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,33 @@
-$(document).ready(function(){
-   $("li.dropdown").click(function(){
-     $(this).find(".dropdowncontent").toggle();
-   });
+// $(document).ready(function(){
+//    $("li.dropdown").click(function(){
+//      $(this).find(".dropdowncontent").toggle();
+//    });
+//
+//    $(".menu-toggle").click(function(){
+//      $(".navright").toggleClass("active");
+//    });
+//
+//  });
 
-   $(".menu-toggle").click(function(){
-     
-     $(".navright").toggleClass("active");
-   });
+//versione alternativa
+ $(".dropdown").click(function() {
+   //mi salvo tutti i .dropdown presenti in pagina
+   var drop = $(".dropdown");
+   console.log($(this));
+   for(var i = 0; i < drop.length; i++) {
+     console.log("entro nel for", drop.eq(i));
+     //se il drop su cui clicco è uguale all'i-esimo drop ciclato allora il valore attuale del contatore i
+     //posso usarlo come indice per risalire al dropdowncontent corispondente
+     if ($(this).is(drop.eq(i))) {
+       console.log("entro nell'if");
+      $("dropdown.visible").removeClass("visible");
+       $(".dropdowncontent").eq(i).toggle();
+     }
+   }
+
 
  });
+
+ $(".menu-toggle").click(function(){
+     $(".navright").toggleClass("active");
+   });
